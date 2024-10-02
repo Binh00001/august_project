edd937ce24c5281679066c4536f95941987c1c08
Binh00001 <136829309+Binh00001@users.noreply.github.com>
2024-10-02 09:27:36 +0700

    change base url

--- lib/utill/app_constants.dart
+++ lib/utill/app_constants.dart
@@ -2,7 +2,7 @@ import 'package:flutter_project_august/models/staff_model.dart';
 import 'package:flutter_project_august/models/user_model.dart';
 
 class AppConstants {
-  static const String baseUrl = "http://18.141.222.147:3001";
+  static const String baseUrl = "http://103.124.93.44:3008";
   static const String token = 'token';
   static const String loginUri = "/v1/auth/login";
   static User defaultUser = User(
